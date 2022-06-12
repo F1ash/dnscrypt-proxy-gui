1ec02e4e12d5cad7a2e66128b5ff75c0011292a7
F1ash <kaperang07@gmail.com>
2022-06-12 13:27:17 +0300

    some fixes for building on old kauth releases;

--- src/helpers/test_helper/dnscrypt_client_test_helper.h
+++ src/helpers/test_helper/dnscrypt_client_test_helper.h
@@ -1,9 +1,13 @@
-#include <kauth/kauthcore_export.h>
-#if KAUTHCORE_ENABLE_DEPRECATED_SINCE(5, 92)
-#include <kauth/actionreply.h>
-#include <kauth/helpersupport.h>
+#if defined(KAUTHCORE_ENABLE_DEPRECATED_SINCE)
+    #include <kauth.h>
 #else
-#include <kauth.h>
+    #include <kauth/kauthcore_export.h>
+    #if KAUTHCORE_ENABLE_DEPRECATED_SINCE(5, 92)
+        #include <kauth/actionreply.h>
+        #include <kauth/helpersupport.h>
+    #else
+        #include <kauth.h>
+    #endif
 #endif
 using namespace KAuth;
 
